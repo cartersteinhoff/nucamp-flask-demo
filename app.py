2d7e7dbfdf85aa6f1e5d715bb74b8615acd3854b
Carter <cartersteinhoff@gmail.com>
2023-07-02 18:01:05 -0700

    updated files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 url = "postgres://fdrbblou:HaA31-x3CBhVBXd2FoyrrQv9yygmwh4-@heffalump.db.elephantsql.com/fdrbblou"
@@ -12,7 +12,6 @@ def root():
     cursor = conn.cursor()
     cursor.execute(
         "SELECT username, email FROM users;")
-    # conn.commit()
     users = cursor.fetchall()
     cursor.close()
     conn.close()
@@ -43,3 +42,24 @@ def submit():
     conn.close()
 
     return render_template('new_user.html', username=username, email=email)
+
+
+@app.route("/delete", methods=['POST'])
+# Route Handler
+def delete():
+    email = request.form['email']
+
+    conn = psycopg2.connect(url)
+    cursor = conn.cursor()
+
+    # Define the DELETE statement
+    delete_query = f"DELETE FROM users WHERE email = '{email}';"
+    # Execute the DELETE statement
+    cursor.execute(delete_query)
+    # Commit the changes to the database
+    conn.commit()
+    # Close the cursor and the database connection
+    cursor.close()
+    conn.close()
+
+    return redirect(url_for('root'))
